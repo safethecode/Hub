7353a2e0c2bc5c82b41d166d12ac405614b0f645
safethecode <iam@sonjim.in>
2023-01-10 04:01:47 +0900

    fat(pages/_app): _app 내부에 QueryClientProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import type { AppProps } from 'next/app';
 import { globalStyles } from 'styles/globalStyles';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
 function App({ Component, pageProps }: AppProps) {
   // Global styles defined in `styles/globalStyles.ts`
   globalStyles();
+
+  // Create a QueryClient
+  // https://react-query-v3.tanstack.com/quick-start
+  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
