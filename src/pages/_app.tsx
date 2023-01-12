b12f36fe061dc89a9edb0ae80b46960ddc5dbabc
safethecode <iam@sonjim.in>
2023-01-12 22:51:27 +0900

    chore(deps): react-query 의존성 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,13 @@
 import type { AppProps } from 'next/app';
 import { globalStyles } from 'styles/globalStyles';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 
 function App({ Component, pageProps }: AppProps) {
   // Global styles defined in `styles/globalStyles.ts`
   globalStyles();
-
-  // Create a QueryClient
-  // https://react-query-v3.tanstack.com/quick-start
-  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+      <Component {...pageProps} />
     </RecoilRoot>
   );
 }
